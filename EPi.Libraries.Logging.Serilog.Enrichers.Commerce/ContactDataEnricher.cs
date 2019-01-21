f481577be91acbf51d9aa6b5ac8a69bdf1029c51
Jeroen Stemerdink <jeroen.development@outlook.com>
2019-01-21 16:48:16 +0100

    Some code cleanup

--- EPi.Libraries.Logging.Serilog.Enrichers.Commerce/ContactDataEnricher.cs
+++ EPi.Libraries.Logging.Serilog.Enrichers.Commerce/ContactDataEnricher.cs
@@ -1,30 +1,34 @@
-// Copyright © 2019 Jeroen Stemerdink.
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation
-// files (the "Software"), to deal in the Software without
-// restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following
-// conditions:
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ContactDataEnricher.cs" company="Jeroen Stemerdink">
+//      Copyright © 2019 Jeroen Stemerdink.
+//      Permission is hereby granted, free of charge, to any person obtaining a copy
+//      of this software and associated documentation files (the "Software"), to deal
+//      in the Software without restriction, including without limitation the rights
+//      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//      copies of the Software, and to permit persons to whom the Software is
+//      furnished to do so, subject to the following conditions:
+//
+//      The above copyright notice and this permission notice shall be included in all
+//      copies or substantial portions of the Software.
+//
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//      SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace EPi.Libraries.Logging.Serilog.Enrichers.Commerce
 {
     using System;
 
+    using Mediachase.Commerce.Customers;
+
     using global::Serilog.Core;
     using global::Serilog.Events;
-
-    using Mediachase.Commerce.Customers;
 
     /// <summary>
     /// Class CommerceDataEnricher.
@@ -32,7 +36,6 @@ namespace EPi.Libraries.Logging.Serilog.Enrichers.Commerce
     /// <seealso cref="ILogEventEnricher" />
     public class ContactDataEnricher : ILogEventEnricher 
     {
-
         /// <summary>
         /// The current contact property name
         /// </summary>
--- EPi.Libraries.Logging.Serilog.Enrichers.Commerce/DefaultCartDataEnricher.cs
+++ EPi.Libraries.Logging.Serilog.Enrichers.Commerce/DefaultCartDataEnricher.cs
@@ -1,22 +1,26 @@
-// Copyright © 2019 Jeroen Stemerdink.
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation
-// files (the "Software"), to deal in the Software without
-// restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following
-// conditions:
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultCartDataEnricher.cs" company="Jeroen Stemerdink">
+//      Copyright © 2019 Jeroen Stemerdink.
+//      Permission is hereby granted, free of charge, to any person obtaining a copy
+//      of this software and associated documentation files (the "Software"), to deal
+//      in the Software without restriction, including without limitation the rights
+//      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//      copies of the Software, and to permit persons to whom the Software is
+//      furnished to do so, subject to the following conditions:
+//
+//      The above copyright notice and this permission notice shall be included in all
+//      copies or substantial portions of the Software.
+//
+//      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//      SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
 namespace EPi.Libraries.Logging.Serilog.Enrichers.Commerce
 {
     using System;
@@ -25,13 +29,13 @@ namespace EPi.Libraries.Logging.Serilog.Enrichers.Commerce
     using EPiServer.Commerce.Order;
     using EPiServer.ServiceLocation;
 
-    using global::Serilog.Core;
-    using global::Serilog.Events;
-
     using Mediachase.Commerce.Customers;
     using Mediachase.Commerce.Orders;
 
     using Newtonsoft.Json;
+
+    using global::Serilog.Core;
+    using global::Serilog.Events;
 
     /// <summary>
     /// Class DefaultCartDataEnricher.
@@ -73,8 +77,9 @@ namespace EPi.Libraries.Logging.Serilog.Enrichers.Commerce
                 return;
             }
 
-            ICart cart = orderRepository.Load<ICart>(customerId: customerContext.CurrentContactId, name: Cart.DefaultName)
-                .FirstOrDefault();
+            ICart cart = orderRepository.Load<ICart>(
+                customerId: customerContext.CurrentContactId,
+                name: Cart.DefaultName).FirstOrDefault();
 
             if (cart != null)
             {
